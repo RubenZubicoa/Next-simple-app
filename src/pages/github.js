d518c70ff751913f690360fab92f962bb8caee3a
Ruben Zubicoa <rubenzubicoatic@gmail.com>
2021-03-04 18:43:22 +0100

    create a basic app

---
+++ src/pages/github.js
@@ -0,0 +1,9 @@
+import Layout from "../components/layout"
+
+const github = () => (
+    <Layout>
+        <h1>Github</h1>
+    </Layout>
+)
+
+export default github
---
+++ src/pages/index.js
@@ -0,0 +1,9 @@
+import Layout from '../components/layout';
+
+const index = () => (
+    <Layout>
+        <h1>Hello world</h1>
+    </Layout>
+)
+
+export default index
